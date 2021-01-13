b37e5bd51cffc66290209b071993172aeb49e249
김민섭 <33247303+meeansub@users.noreply.github.com>
2021-01-13 18:23:29 +0900

    project init

---
+++ src/main/java/com/studylog_server/StudylogServerApplication.java
@@ -0,0 +1,13 @@
+package com.studylog_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudylogServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudylogServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/studylog_server/StudylogServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studylog_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudylogServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
